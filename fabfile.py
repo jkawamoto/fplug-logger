bd3a64aeee6c7268d0958b39f6cecf9989a43f02
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-03-16 20:30:34 +0900

    Update to a generic fabfile.

--- fabfile.py
+++ fabfile.py
@@ -12,21 +12,23 @@ from fabric.api import *
 from fabric.contrib.project import rsync_project
 env.use_ssh_config = True
 
+PACKAGE = "fplug-logger"
+
 @task
 def build():
     """ Build a Docker image.
     """
-    run("mkdir -p fplug-logger")
-    rsync_project(local_dir=".", remote_dir="fplug-logger")
-    with cd("fplug-logger"):
-        run("docker build -t jkawamoto/fplug-logger .")
+    run("mkdir -p " + PACKAGE)
+    rsync_project(local_dir=".", remote_dir=PACKAGE, exclude=['.git'])
+    with cd(PACKAGE):
+        run("docker build -t jkawamoto/{package} .".format(package=PACKAGE))
 
 @task
 def rpi_build():
     """ Build a Docker image for Raspberry Pi.
     """
-    run("mkdir -p fplug-logger")
-    rsync_project(local_dir=".", remote_dir="fplug-logger")
-    with cd("fplug-logger"):
+    run("mkdir -p " + PACKAGE)
+    rsync_project(local_dir=".", remote_dir=PACKAGE, exclude=['.git'])
+    with cd(PACKAGE):
         run("mv Dockerfile.rpi Dockerfile")
-        run("docker build -t jkawamoto/rpi-fplug-logger .")
+        run("docker build -t jkawamoto/rpi-{package} .".format(package=PACKAGE))
